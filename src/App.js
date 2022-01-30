df9652052e62a94c153504e2c92d626b77aab4ee
suyeon-hong <bellahong95@gmail.com>
2022-01-30 20:48:48 +0900

    :sparkles: 등록버튼 클릭시 리스트 추가 기능 구현
    See issues: #2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 function App() {
+  const [userName, setUserName] = useState('')
+  const [userNumber, setUserNumber] = useState('')
+  const [list, setList] = useState([])
+  const checkName = (e) => {
+    setUserName(e.target.value)
+  }
+  const checkNumber = (e) => {
+    setUserNumber(e.target.value)
+  }
+  const changeHandler = (e) => {
+    e.preventDefault()
+
+    if (userName === '' || userNumber === '') {
+      alert('값을 입력해 주세요.')
+      return
+    }
+    setList((current) => {
+      setList([...current, { name: userName, number: userNumber }])
+    })
+    setUserName('')
+    setUserNumber('')
+  }
   return (
     <>
       <div className='userInput'>
         <form>
-          <input className='input' type="text" placeholder='이름' />
-          <input className='input' type="text" placeholder='전화번호' />
-          <button className='btn'>등록</button>
+          <input className='input' type="text" value={userName} onChange={checkName} placeholder='이름' />
+          <input className='input' type="text" value={userNumber} onChange={checkNumber} placeholder='전화번호' />
+          <button className='btn' onClick={changeHandler}>등록</button>
         </form>
         <input className='input' type="text" placeholder='검색 할 이름을 입력하세요' />
       </div>
       <ul className='listBox'>
-        <li className='list'>
-          <h1 className='name'>김민준</h1>
-          <p className='number'>010-0000-0000</p>
-          <button className='btn'>수정</button>
-          <button className='btn'>삭제</button>
-        </li>
+        {list && list.map((li) => {
+          return (
+            <li className='list' key={li.name}>
+              <h1 className='name'>{li.name}</h1>
+              <p className='number'>{li.number}</p>
+              <button className='btn'>수정</button>
+              <button className='btn'>삭제</button>
+            </li>
+          )
+        })}
       </ul>
     </>
   )
