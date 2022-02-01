fe4a3ea58edf3651d3d30cdf383619cb1a6b02d6
suyeon-hong <bellahong95@gmail.com>
2022-02-01 16:09:05 +0900

    :sparkles: 수정,삭제 기능, 검색어 필터링 기능 추가

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
         name: '강땡땡',
         number: '010-0000-0002'
       }
-    ]
+    ],
+    keyword: ''
   }
 
   getUserInput = (newInput) => {
@@ -27,11 +28,30 @@ class App extends Component {
     })
   }
 
+  onRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+
+  itemUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({ information: information.map(info => info.id === id ? { ...info, ...data } : info) })
+  }
+
+  onSearch = (keyword) => {
+    this.setState({ keyword: keyword })
+  }
+
   render() {
+    const { information, keyword } = this.state;
+    const filteredList = information.filter(info => info.name.indexOf(keyword) !== -1)
+
     return (
       <>
-        <UserInput getUserInput={this.getUserInput} />
-        <ListBox data={this.state.information} />
+        <UserInput getUserInput={this.getUserInput} onSearch={this.onSearch} />
+        <ListBox data={filteredList} onRemove={this.onRemove} onUpdate={this.itemUpdate} />
       </>
     )
   }
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -19,7 +19,11 @@ class UserInput extends Component {
 			[e.target.name]: e.target.value,
 		});
 	};
+	handleSearch = (e) => {
+		this.props.onSearch(e.target.value);
+	};
 	render() {
+		const { name, number } = this.state;
 		return (
 			<div className="userInput">
 				<form onSubmit={this.handleSubmit}>
@@ -27,7 +31,7 @@ class UserInput extends Component {
 						onChange={this.handleChange}
 						className="input"
 						type="text"
-						value={this.state.name}
+						value={name}
 						placeholder="이름"
 						name="name"
 					></input>
@@ -35,7 +39,7 @@ class UserInput extends Component {
 						onChange={this.handleChange}
 						className="input"
 						type="text"
-						value={this.state.number}
+						value={number}
 						placeholder="전화번호"
 						name="number"
 					></input>
@@ -44,8 +48,10 @@ class UserInput extends Component {
 					</button>
 				</form>
 				<input
+					onChange={this.handleSearch}
 					className="input"
 					type="text"
+					name="keyword"
 					placeholder="이름을 검색해 주세요."
 				/>
 			</div>
